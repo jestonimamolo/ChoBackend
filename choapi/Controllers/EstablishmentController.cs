db805b877c36b66922227c69887990c26c398efa
Mark Saile <mj.saile@gukodigital.com>
2024-06-03 22:00:33 +0800

    Converts meters to KM for distance

--- choapi/Controllers/EstablishmentController.cs
+++ choapi/Controllers/EstablishmentController.cs
@@ -1849,7 +1849,8 @@ namespace choapi.Controllers
             var num2 = establishmentLongitude * (Math.PI / 180.0) - num1;
             var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) +
                      Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
-            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+            var meters = 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+            return meters / 1000;
         }
     }
 }
